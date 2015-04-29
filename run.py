e838f6ce44dd75793a7d6895e2ce938394692905
SietseHuisman <sietsehuisman@hotmail.com>
2015-04-29 22:33:40 +0200

    modified run.py

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
-from solver import Solver
+from solver_v2 import Solver
 
 s = Solver()
 
-s.display(s.parse_grid(s.example))
+#s.display(s.parse_grid(s.example))
+
+constraints = s.construct_constraints()
+variables = s.parse_grid(s.example)
+propagates_variables = s.propagate(constraints, variables)
+propagates_variablesOLD = s.propagate(constraints, variables)
+
+s.solve(constraints, propagates_variables, propagates_variablesOLD)
