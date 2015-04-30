86545ad97b056f06cc4eae00c97ed19c84271b55
SietseHuisman <sietsehuisman@hotmail.com>
2015-04-30 18:38:08 +0200

    updated, it works!

--- run.py
+++ run.py
@@ -7,8 +7,18 @@ s = Solver()
 constraints = s.construct_constraints()
 variables = s.parse_grid(s.example)
 propagates_variables = s.propagate(constraints, variables)
-propagates_variablesOLD = list(propagates_variables)
+#propagates_variablesOLD = list(propagates_variables)
 
+propagates_OLDvariables = []
+for element in variables:
+    propagates_OLDvariables.append(list(element))
+    
+#OLDvariables = list(variables)
+
+'''
+print id(OLDvariables[0])
+print id(variables[0])
+'''
 '''
 print propagates_variables
 print propagates_variablesOLD
@@ -17,5 +27,5 @@ print propagates_variables
 print propagates_variablesOLD
 '''
 
-
-s.solve(constraints, propagates_variables, propagates_variablesOLD)
+#s.solve(constraints, variables, OLDvariables)
+s.solve(constraints, propagates_variables, propagates_OLDvariables)
--- solver_v2.py
+++ solver_v2.py
@@ -2,10 +2,10 @@ import time
 
 class Solver:
 	def __init__(self):
-		self.example = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
-		self.display(self.example)
-		#self.example = '81.2.3.6.29715.3483568......8.621.7474938.126162.7983.638514792921768............'
-			#every square can take only one value
+		#self.example = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+		#self.example = '.142935672971563483568472195836219747493851261624798356385147929217684...........'
+		self.example = '.14...56...715.......847...58.......749.85.26.624.983.63851...2921...4...........'
+		#self.display(self.example)
 			
 	def parse_grid(self,grid):
 		variables = []
@@ -14,13 +14,11 @@ class Solver:
 				variables.append([int(c)])
 			else:
 				variables.append([i for i in range(1,10)])
-			
 		assert len(variables) == 9 ** 2
 		return variables
 	
 	def construct_constraints(self):
 		constraints = []
-		
 		#rows
 		count = 1
 		for i in range(1,10):
@@ -37,6 +35,15 @@ class Solver:
 			for j in range(0,9):
 				sublist.append((count + 9 * j) -1)
 			constraints.append(sublist)
+		
+		#subdivisions
+		for l in 0,27,54:
+			for i in 0,3,6:
+				grid = []
+				for j in 0,9,18:
+					for k in range(1,4):
+						grid.append((i+j+k+l)-1)
+				constraints.append(grid)	
 		return constraints
 	
 	def check_constraints(self, constraints, variables):
@@ -46,44 +53,30 @@ class Solver:
 					if variables[value1] != 0 and variables[value2] !=0 and (variables[value1] == variables[value2]) and value1 != value2:
 						#return false if constraint is violated
 						return False
-		
 		return True
 	
 	
 	def solve(self, constraints, variables, OLDvariables):
-		
 		solution = [0] * 81
 		depth = 0
 		k = 0
 		
-		
-		
 		while depth < 81:
-			
 			while variables[depth] == []:
-				
-				if depth < 10:
-					print depth
-				variables[depth] = list(OLDvariables[depth])
-				
+				variables[depth] = OLDvariables[depth][:]
 				solution[depth] = 0
-				variables[depth -1] = variables[depth-1][1:]
-				#variables[depth -1].remove(variables[depth -1][0])
+				variables[depth -1].remove(variables[depth -1][0])
 				depth -=1
-				
 			
 			solution[depth] = variables[depth][0]
 			if self.check_constraints(constraints, solution):
 				depth +=1
 			else:
+				
 				variables[depth].remove(variables[depth][0])
-			#print solution
-		
+			
 		self.display(solution)
-		
 		return solution
-		
-		
 	
 	
 	def propagate(self, constraints, variables):
@@ -94,7 +87,6 @@ class Solver:
 					if key in constraint:
 						for variable in constraint:
 							if values[0] in variables[variable] and values != variables[variable]:
-								#print 'removing ' + str(values[0]) + "from " + str(variables[variable] )
 								variables[variable].remove(values[0])
 			key +=1
 		return variables	
